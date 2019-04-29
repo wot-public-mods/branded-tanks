d8de19b2ad3cbdf99bacec928dbe6bd442c50779
Andrey Andruschyshyn <p0lir0id@yandex.ru>
2019-04-29 10:01:43 +0300

    reworked utils

--- python/gui/branding/utils.py
+++ python/gui/branding/utils.py
@@ -4,27 +4,34 @@ import ResMgr
 __all__ = ('byteify', 'override', 'readFromVFS', 'getFashionValue', 'parseLangFields', 'getHangarVehicle',
 		'getIconPatch', 'readBrandingItem', )
 
-def override(holder, name, target=None):
-	"""using for override any staff"""
-	if target is None:
-		return lambda target: override(holder, name, target)
-	original = getattr(holder, name)
-	overrided = lambda *a, **kw: target(original, *a, **kw)
-	if not isinstance(holder, types.ModuleType) and isinstance(original, types.FunctionType):
-		setattr(holder, name, staticmethod(overrided))
-	elif isinstance(original, property):
-		setattr(holder, name, property(overrided))
+def override(holder, name, wrapper=None, setter=None):
+	"""Override methods, properties, functions, attributes
+	:param holder: holder in which target will be overrided
+	:param name: name of target to be overriden
+	:param wrapper: replacement for override target
+	:param setter: replacement for target property setter"""
+	if wrapper is None:
+		return lambda wrapper, setter=None: override(holder, name, wrapper, setter)
+	target = getattr(holder, name)
+	wrapped = lambda *a, **kw: wrapper(target, *a, **kw)
+	if not isinstance(holder, types.ModuleType) and isinstance(target, types.FunctionType):
+		setattr(holder, name, staticmethod(wrapped))
+	elif isinstance(target, property):
+		prop_getter = lambda *a, **kw: wrapper(target.fget, *a, **kw)
+		prop_setter = target.fset if not setter else lambda *a, **kw: setter(target.fset, *a, **kw)
+		setattr(holder, name, property(prop_getter, prop_setter, target.fdel))
 	else:
-		setattr(holder, name, overrided)
+		setattr(holder, name, wrapped)
 
 def byteify(data):
-	"""using for convert unicode key/value to utf-8"""
+	"""Encodes data with UTF-8
+	:param data: Data to encode"""
 	result = data
-	if isinstance(data, types.DictType):
+	if isinstance(data, dict):
 		result = {byteify(key): byteify(value) for key, value in data.iteritems()}
-	elif isinstance(data, (set, tuple, types.ListType)):
+	elif isinstance(data, (list, tuple, set)):
 		result = [byteify(element) for element in data]
-	elif isinstance(data, types.UnicodeType):
+	elif isinstance(data, unicode):
 		result = data.encode('utf-8')
 	return result
 
