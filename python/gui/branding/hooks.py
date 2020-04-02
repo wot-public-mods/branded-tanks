9bce588f4e2a3a23f756cbda23b7427e2dea1962
Andrey Andruschyshyn <p0lir0id@yandex.ru>
2020-04-03 01:25:25 +0300

    fixed situation when vehicles don't customized

--- python/gui/branding/hooks.py
+++ python/gui/branding/hooks.py
@@ -8,11 +8,20 @@ from vehicle_systems.CompoundAppearance import CompoundAppearance
 
 __all__ = ()
 
+# change vehicle outfit (may dont work if arena data not ready)
 @override(CompoundAppearance, '_prepareOutfit')
-def applyVehicleOutfit(baseMethod, baseInstance, outfitCD):
+def _prepareOutfit(baseMethod, baseInstance, outfitCD):
 	from gui.branding.controllers import g_controllers
 	outfit = g_controllers.vehicle.getVehicleOutfit(baseInstance, baseInstance.outfit)
 	return outfit or baseMethod(baseInstance, outfitCD)
+
+# this need for fix vehicles that dont changed during arena not ready
+@override(CompoundAppearance, '_CompoundAppearance__applyVehicleOutfit')
+def _applyVehicleOutfit(baseMethod, baseInstance):
+	from gui.branding.controllers import g_controllers
+	outfit = g_controllers.vehicle.getVehicleOutfit(baseInstance, baseInstance.outfit)
+	baseInstance._CommonTankAppearance__outfit = outfit or baseInstance.outfit
+	return baseMethod(baseInstance)
 
 # modsListApi
 g_modsListApi = None
