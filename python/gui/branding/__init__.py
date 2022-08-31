d0d60ff7d9e97ca067f4a26722a87429c8755d6a
Andrii Andrushchyshyn <poliroid@pm.me>
2022-09-01 01:15:27 +0300

    credentials

--- python/gui/branding/__init__.py
+++ python/gui/branding/__init__.py
@@ -1,10 +1,10 @@
-__author__ = "Andruschyshyn Andrey"
+__author__ = "Andrii Andrushchyshyn"
 __copyright__ = "Copyright 2022, poliroid"
-__credits__ = ["Andruschyshyn Andrey"]
+__credits__ = ["Andrii Andrushchyshyn"]
 __license__ = "CC BY-NC-SA 4.0"
 __version__ = "4.2.0"
-__maintainer__ = "Andruschyshyn Andrey"
-__email__ = "poliroid@pm.me"
+__maintainer__ = "Andrii Andrushchyshyn"
+__email__ = "contact@poliroid.me"
 __status__ = "Production"
 
 from gui.branding.data import *
